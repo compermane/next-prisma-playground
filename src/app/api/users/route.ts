46373b1810b58dada4309ad0bf79741ea11f187c
Eugênio Nishimiya <eugenio.nishimiya@profusion.mobi>
2025-08-28 16:35:03 -0300

    feat: handle database data

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,8 @@
-import prisma from "@/lib/prisma";
+import prisma from "../../../lib/prisma"
 
-export async function GET() {
+const GET = async () => {
   const users = await prisma.user.findMany();
   return new Response(JSON.stringify(users), { status: 200 });
 }
+
+export default GET
