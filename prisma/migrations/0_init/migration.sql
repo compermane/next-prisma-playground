9e5f611281d9559203737b9ca6121eceed13f226
Eugênio Nishimiya <eugenio.nishimiya@profusion.mobi>
2025-08-28 16:11:02 -0300

    feat: add prisma

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `senha` VARCHAR(255) NOT NULL,
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
